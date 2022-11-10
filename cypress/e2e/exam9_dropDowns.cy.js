4f0d49994a5e6e5afa9d601ebda42fd2a3df1d70
trgy2145 <trgy2145@gmail.com>
2022-11-10 22:55:37 +0100

    dropdowns is finished

--- cypress/e2e/exam9_dropDowns.cy.js
+++ cypress/e2e/exam9_dropDowns.cy.js
@@ -11,5 +11,34 @@ describe("Dropdowns", ()=>{
         cy.visit("https://www.dummyticket.com/dummy-ticket-for-visa-application/")
         cy.get("#select2-billing_country-container").click()
         cy.get('.select2-search__field').type('Italy').type('{enter}')
+        cy.get("#select2-billing_country-container").should('have.text','Italy') // burda text value degil..
     })
+      it("Auto suggest dropdown", () => {
+        
+        cy.visit("https://www.wikipedia.org/")
+        cy.get('#searchInput').type('Delhi')
+        cy.get('.suggestion.title').contains('Delhi University').click() // cıkan tavsiyelerin sayısı bu classla aldık
+
+       
+      });
+         it("dynamic dropdown", () => {
+           cy.visit("https://www.google.org/");
+           cy.get("input[name='q']").type("cypress automation");
+
+           cy.wait(2000)
+           cy.get("div.wM6W7d>span").should("have.length","11")
+           // cy.get("div.wM6W7d>span").contains("cypress automation").click(); 
+           cy.get("div.wM6W7d>span").each(($el,index,$list)=>{ // bir üstteki kod ile aynı işi yapar
+            if($el.text() ==='cypress automation tutorial'){
+                cy.wrap($el).click() //el element demek 
+            }
+           })
+
+           cy.get("input[name='q'").should(
+             "have.value",
+             "cypress automation tutorial"
+           );
+        
+        
+        });
 })
