1d60858ec5b46d7a49ee320bd6507cc7a3f72db1
trgy2145 <trgy2145@gmail.com>
2022-11-16 23:33:12 +0100

    handling tables

--- cypress/e2e/exam12_iframe.cy.js
+++ cypress/e2e/exam12_iframe.cy.js
@@ -1,24 +1,25 @@
-import "cypress-iframe"; 
+import "cypress-iframe";
 
-describe('Iframe',()=>{
-    it('method one',()=>{
-        cy.visit("http://the-internet.herokuapp.com/iframe")
-        const iframe = cy.get("#mce_0_ifr")
-                .its('0.contentDocument.body')
-                .should('be.visible')
-                .then(cy.wrap)
-            iframe.clear().type("merhaba {ctrl+a}")
-            cy.get("[aria-label='Bold']").click()
-    })
-    it('method two- by using custom method',()=>{
-        cy.visit("http://the-internet.herokuapp.com/iframe");
-        cy.getIframe("#mce_0_ifr").clear().type("hacı naber");
-        cy.get("[aria-label='Bold']").click();
-    })
-    it.only("method three- by using iframe plugin", () => {
-      cy.visit("http://the-internet.herokuapp.com/iframe");
-      cy.frameLoaded("#mce_0_ifr"); // sadece yukler interaktif değil
-      cy.iframe("#mce_0_ifr").clear().type("bu da güzel");
-       cy.get("[aria-label='Bold']").click();
-    });
-})
+describe("Iframe", () => {
+  it("method one", () => {
+    cy.visit("http://the-internet.herokuapp.com/iframe");
+    const iframe = cy
+      .get("#mce_0_ifr")
+      .its("0.contentDocument.body")
+      .should("be.visible")
+      .then(cy.wrap);
+    iframe.clear().type("merhaba {ctrl+a}");
+    cy.get("[aria-label='Bold']").click();
+  });
+  it("method two- by using custom method", () => {
+    cy.visit("http://the-internet.herokuapp.com/iframe");
+    cy.getIframe("#mce_0_ifr").clear().type("hacı naber");
+    cy.get("[aria-label='Bold']").click();
+  });
+  it.only("method three- by using iframe plugin", () => {
+    cy.visit("http://the-internet.herokuapp.com/iframe");
+    cy.frameLoaded("#mce_0_ifr"); // sadece yukler interaktif değil
+    cy.iframe("#mce_0_ifr").clear().type("bu da güzel"); // bu interaktiftir
+    cy.get("[aria-label='Bold']").click();
+  });
+});
