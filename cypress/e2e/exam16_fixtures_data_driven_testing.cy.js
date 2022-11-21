d813c3583777b9d8de998de6a393d6058e1d46f0
trgy2145 <trgy2145@gmail.com>
2022-11-21 21:48:08 +0100

    the end of fixtures coding

--- cypress/e2e/exam16_fixtures_data_driven_testing.cy.js
+++ cypress/e2e/exam16_fixtures_data_driven_testing.cy.js
@@ -15,7 +15,7 @@ describe("Fixtures", () => {
   }); */
 
   //! by using hook ,take the data
-  let userData;
+  /* let userData;
   before(() => {
     cy.fixture("example.json").then((data) => {
       userData = data;
@@ -32,5 +32,30 @@ describe("Fixtures", () => {
         userData.expected
       );
     });
-  });
+  }); */
+
+  it('Data driven testing',()=>{
+    cy.fixture('example2').then((data)=>{
+        cy.visit("https://opensource-demo.orangehrmlive.com/");
+        data.forEach((userData) => {
+
+            cy.get("input[placeholder='Username']").type(userData.name);
+            cy.get("input[placeholder='Password']").type(userData.pass);
+            cy.get("button[type='submit']").click();
+            if (userData.name == "Admin" && userData.pass=='admin123') {
+                cy.wait(5000);
+                cy.get(":nth-child(2) > .oxd-main-menu-item").click();
+                cy.get(".oxd-topbar-header-breadcrumb > .oxd-text").should(
+                  "have.text",
+                  userData.expected
+                );
+            }
+            else{
+                cy.get(".oxd-text.oxd-text--p.oxd-alert-content-text").should('have.text',userData.expected)
+            }
+
+        });
+
+    })
+  })
 });
